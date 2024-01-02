28e6c3a42352bb5f2b0b2105796ddae91088d5db
EC2 Default User <ec2-user@ip-172-31-27-47.ap-northeast-1.compute.internal>
2024-01-02 16:13:05 +0000

    [Update]Task3-l29

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :profile_image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +7,11 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   get 'homes/top'
-  get 'homes/about'
+  get 'homes/about' => 'homes#about', as:'about'
   devise_for :users
   root to: 'homes#top'
-  resources :users, only: [:new, :create, :show, :index]
-  resources :bookers,only: [:new, :create, :show, :index]
+  resources :users, only: [:show, :edit]
+  resources :books,only: [:new, :create, :show, :index, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
